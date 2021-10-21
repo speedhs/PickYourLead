3dbfa87e4026831136938bc5dd9956f6ddb7cbed
dgdevanshi <89896262+dgdevanshi@users.noreply.github.com>
2021-10-21 21:52:08 +0530

    dropdown list

--- app/src/main/java/com/example/pickyourlead/Register.java
+++ app/src/main/java/com/example/pickyourlead/Register.java
@@ -3,6 +3,8 @@ package com.example.pickyourlead;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Register.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.branches));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
