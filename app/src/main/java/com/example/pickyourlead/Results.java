a2feddc67a7b25c800ade3a05346554174a44d05
Tushar Mishra <tusharmishra16@gmail.com>
2021-11-15 22:35:47 +0530

    T

--- app/src/main/java/com/example/pickyourlead/Results.java
+++ app/src/main/java/com/example/pickyourlead/Results.java
@@ -24,32 +24,32 @@ public class Results extends AppCompatActivity {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-        textviewone=findViewById(R.id.textView9);
-        textviewtwo=findViewById(R.id.textView13);
-        db.collection("trial").document("players").get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        if (documentSnapshot.exists()) {
-                          Long vScore=documentSnapshot.getLong("VIRAT");
-                            Long rScore=documentSnapshot.getLong("ROHIT");
-//                            String rScore = (String) documentSnapshot.get("ROHIT");
-                            //Map<String, Object> note = documentSnapshot.getData();
-
-                            textviewone.setText("Virat got "+vScore+" votes");
-                            textviewtwo.setText("Rohit got "+rScore+" votes");
-
-                        } else {
-                            Toast.makeText(Results.this, "Document does not exist", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Results.this, "Error!", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, e.toString());
-                    }
-                });
+//        textviewone=findViewById(R.id.textView9);
+//        textviewtwo=findViewById(R.id.textView13);
+//        db.collection("trial").document("players").get()
+//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                        if (documentSnapshot.exists()) {
+//                          Long vScore=documentSnapshot.getLong("VIRAT");
+//                            Long rScore=documentSnapshot.getLong("ROHIT");
+////                            String rScore = (String) documentSnapshot.get("ROHIT");
+//                            //Map<String, Object> note = documentSnapshot.getData();
+//
+//                            textviewone.setText("Virat got "+vScore+" votes");
+//                            textviewtwo.setText("Rohit got "+rScore+" votes");
+//
+//                        } else {
+//                            Toast.makeText(Results.this, "Document does not exist", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(Results.this, "Error!", Toast.LENGTH_SHORT).show();
+//                        Log.d(TAG, e.toString());
+//                    }
+//                });
     }
 }
